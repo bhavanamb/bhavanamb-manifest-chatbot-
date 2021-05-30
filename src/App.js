38210fc2d3aab9d2cd89fa2a67bce28389c1c34b
Bhavana <mbbhavana9@gmail.com>
2021-05-30 09:13:58 -0500

    chat box css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatBox from "../src/components/ChatBox/ChatBox";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: auto auto;
+  max-width: 867px;
+  height: 100vh;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <ChatBox />
+    </Wrapper>
   );
 }
 
